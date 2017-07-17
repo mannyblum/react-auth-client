46abf2adbacacea050eb9741d171f58476e7f9f6
Manny Blum <manny.blum@gmail.com>
2017-07-16 22:24:41 -0400

    started creating the actions

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+exports function signinUser({ email, password }) {
+  // submit email/password to server
+
+  // If request is good...
+  // - Update state to indicate user is authenticated
+  // - Save the JWT token
+  // - redirect to the route '/feature'
+
+  // If request is bad ...
+  // - Show an error to the user
+}
