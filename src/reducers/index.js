d307d4e378df9581906158863c7cee1af385ef93
Manny Blum <manny.blum@gmail.com>
2017-07-18 00:39:38 -0400

    added auth reducer and auth types

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer } from 'redux-form';
+import authReducer from './auth_reducer';
 
 const rootReducer = combineReducers({
-  form: reducer
+  form: reducer,
+  auth: authReducer
 });
 
 export default rootReducer;
