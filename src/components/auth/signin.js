d0429179352b9d0c7ef85218e14e9dea77b51765
Manny Blum <manny.blum@gmail.com>
2017-07-18 01:30:28 -0400

    added alert for auth_error case

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -9,6 +9,7 @@ class Signin extends Component {
     // Need to do something to log user in
     this.props.signinUser({email, password});
   }
+
   renderInput(field) {
     return (
       <div>
@@ -17,10 +18,21 @@ class Signin extends Component {
     );
   }
 
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong>Oooops!</strong> {this.props.errorMessage}
+        </div>
+      );
+    }
+  }
+
   render() {
     // NOTE: This is slightly different from the video because of updates
     // to redux-form
     const { handleSubmit } = this.props;
+
     return (
       <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
         <fieldset className="form-group">
@@ -31,13 +43,18 @@ class Signin extends Component {
           <label>Password:</label>
           <Field name="password" component={this.renderInput} type="password" />
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign in</button>
       </form>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
 // reduxForm no longer uses array to keep track of fields, need connect
 // to hook in actions
 const form = reduxForm({ form: 'signin' })(Signin);
-export default connect(null, actions)(form);
+export default connect(mapStateToProps, actions)(form);
