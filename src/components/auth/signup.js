3b0e08b2485bfc4c9de061d9e1cf765e69f98bbe
Manny Blum <manny.blum@gmail.com>
2017-07-19 12:18:04 -0400

    created signup form with fields to display to user with button

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
 class Signup extends Component {
-  render() {
+  handleFormSubmit({ email, password, passwordConfirm }) {
+    this.props.signupUser({ email, password, passwordConfirm });
+  }
+
+  renderInput(field) {
     return (
       <div>
-        Signup
+        <input {...field.input} type={field.type} className="form-control" />
       </div>
+    );
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <Field name="email" component={this.renderInput} type="email" />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <Field name="password" component={this.renderInput} type="password" />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Confirm Password:</label>
+          <Field name="passwordConfirm" component={this.renderInput} type="password" />
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign Up</button>
+      </form>
+
+
     );
   }
 }
 
-export default Signup;
+const signupForm = reduxForm({ form: 'signup' })(Signup);
+export default connect(null, actions)(signupForm);
