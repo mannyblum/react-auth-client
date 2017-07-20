31cc048d6afe4b30e10f13944e7b94a60df0e4e9
Manny Blum <manny.blum@gmail.com>
2017-07-19 20:14:07 -0400

    added a HOC to secure the Feature route

---
+++ src/components/feature.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+class Feature extends Component {
+  render() {
+    return (
+      <div>This is a feature</div>
+    );
+  }
+}
+
+export default Feature;
